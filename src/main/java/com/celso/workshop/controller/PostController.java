1da2784ea695d5289f23943e02d4057563f9245c
Celso Bertolotto <celsonyl@hotmail.com>
2022-01-07 13:35:20 -0300

    endpoint get all posts ok

--- src/main/java/com/celso/workshop/controller/PostController.java
+++ src/main/java/com/celso/workshop/controller/PostController.java
@@ -4,6 +4,7 @@ import com.celso.workshop.controller.model.PostRequest;
 import com.celso.workshop.controller.model.PostResponse;
 import com.celso.workshop.translator.PostMapper;
 import com.celso.workshop.usecase.CreatePostUsecase;
+import com.celso.workshop.usecase.GetAllPostsUsecase;
 import com.celso.workshop.usecase.GetPostByIdUsecase;
 import com.celso.workshop.usecase.UpdatePostUsecase;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping(value = "/post")
 @RestController
@@ -23,12 +26,28 @@ public class PostController {
     private GetPostByIdUsecase getPostByIdUsecase;
     @Autowired
     private UpdatePostUsecase updatePostUsecase;
+    @Autowired
+    private GetAllPostsUsecase getAllPostsUsecase;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<PostResponse> getPostById(@PathVariable String id) {
         var post = getPostByIdUsecase.execute(id);
 
         return ResponseEntity.ok().body(new PostMapper().postDomainToResponse(post));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        var posts = getAllPostsUsecase.execute();
+
+        return ResponseEntity.ok().body(posts.stream()
+                .map(new PostMapper()::postDomainToResponse)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping(value = "/user/{id}")
+    public ResponseEntity<List<PostResponse>> getAllPostsByUserId(@PathVariable String id) {
+        return null;
     }
 
     @PostMapping
